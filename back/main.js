28eb68c0e176db2a8a31b1c6f65eea61fdffc7a1
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-05-17 23:21:27 +0200

    express middleware

--- back/main.js
+++ back/main.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+var bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3003
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(cors())
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT + '!')
