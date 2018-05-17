57bae5ae323e94372a93eb17ae6120031178baa6
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-05-17 23:22:40 +0200

    POST /toy logic

--- back/main.js
+++ back/main.js
@@ -19,7 +19,21 @@ app.get('/toy', (req, res) => {
     res.json(JSON.parse(data))
   })
 })
+
 app.post('/toy', (req, res) => {
+  fs.readFile(DB_FILE, FILE_ENCODING, (err, data) => {
+    let toys = JSON.parse(data)
+    let toy = {
+      name: req.body.name,
+      image: req.body.image,
+      description: req.body.description
+    }
+    toys.push(toy)
+    fs.writeFile(DB_FILE, JSON.stringify(toys), (err) => {
+      if (err) throw err
+      res.sendStatus(201)
+    })
+  })
 })
 
 app.listen(PORT, function () {
