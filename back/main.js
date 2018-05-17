b5420498cf97a28307c2e81f35e7d49f81679a8e
Lucas Vancompernolle <lucas.vancompernolle@epitech.eu>
2018-05-17 23:22:13 +0200

    GET /toy logic

--- back/main.js
+++ back/main.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const fs = require('fs')
 const morgan = require('morgan')
 const cors = require('cors')
 var bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3003
+const DB_FILE = "toys.json"
+const FILE_ENCODING = 'utf8'
 
 const app = express()
 
@@ -12,6 +15,9 @@ app.use(morgan('dev'))
 app.use(cors())
 
 app.get('/toy', (req, res) => {
+  fs.readFile(DB_FILE, FILE_ENCODING, (err, data) => {
+    res.json(JSON.parse(data))
+  })
 })
 app.post('/toy', (req, res) => {
 })
